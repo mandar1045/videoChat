892944835616344d3f94ed2e5d5e59acf60ad2af
Mandar <mandarjoshi1045@gmail.com>
2025-09-13 02:55:26 +0530

    db.js commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI environment variable is not set");
+    }
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
